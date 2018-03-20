36777705bc02dc30a473492b3a9524be62a26e81
roni ramon <roniramon260@gmail.com>
2018-03-20 16:19:56 -0700

    ttt game jquery

--- Jquery-Tic-Tac/js/main.js
+++ Jquery-Tic-Tac/js/main.js
@@ -1,6 +1,9 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require("./ttt-view");// require appropriate file
+const Game = require("../solution/game.js");// require appropriate file
 
 $( () => {
   // Your code here
+  const game =  new Game();
+  const $grid = $('.ttt');
+  new View(game, $grid);
 });
--- Jquery-Tic-Tac/js/ttt-view.js
+++ Jquery-Tic-Tac/js/ttt-view.js
@@ -1,11 +1,53 @@
 class View {
-  constructor(game, $el) {}
+  constructor(game, $el) {
+    this.game = game;
+    this.$el = $el;
+    this.setupBoard();
+    this.bindEvents();
+  }
 
-  bindEvents() {}
+  bindEvents() {
+    $('ul.row').on('click', 'li', (e)=>{
+      const pos = $(e.currentTarget).data("data-pos");
+      let player = this.game.currentPlayer;
+      // console.log(pos);
+      this.game.playMove(pos);
+      $(e.currentTarget).css("background-color", "white");
+      $(e.currentTarget).off("mouseenter");
+      $(e.currentTarget).off("mouseout");
+      $(e.currentTarget).text(player);
+      if(this.game.winner()){
+        let winner = this.game.winner();
+        const b = $('p').text(`The Winner is ${winner}`);
+      }
+    });
+  }
 
-  makeMove($square) {}
+  makeMove($square) {
 
-  setupBoard() {}
-}
+  }
+
+  setupBoard() {
+    const board = this.game.board;
+    let row;
+    let square;
+    for (var i = 0; i < board.grid.length; i++) {
+      row = $("<ul>").addClass("row").addClass("group");
+      for (var col = 0; col < board.grid[i].length; col++) {
+        square = $("<li>").addClass("square").data("data-pos", [i, col]);
+        square.on("mouseenter", (e) =>{
+          const $sq = $(e.currentTarget);
+          $sq.css("background-color", "red");
+        });
+        square.on("mouseout", (e) =>{
+          const $sq = $(e.currentTarget);
+          $sq.css("background-color", "blue");
+        });
+        row.append(square);
+      }
+      this.$el.append(row);
+    }
+    }
+  }
 
 module.exports = View;
